4edeb86dcf83673dc94f538c847f52c2487e8f01
HDCat2 <henningjiang888+2@gmail.com>
2023-11-11 18:05:01 -0500

    Setting up attributes

--- src/menu_script.js
+++ src/menu_script.js
@@ -1,3 +1,7 @@
+function handleDownload() {
+    console.log("lmao");
+}
+
 function display_unsupported() {
     document.getElementById("root_div").className = "unsupported";
     document.getElementById("root_div").innerHTML = "This extension does not support this webpage :c";
@@ -5,6 +9,7 @@ function display_unsupported() {
 
 function display_supported() {
     var root_div = document.getElementById("root_div")
+
     var form = document.createElement('form');
 
     var input_bpm = document.createElement('input');
@@ -12,6 +17,11 @@ function display_supported() {
     var input_cs = document.createElement('input');
     var input_od = document.createElement('input');
     var input_hp = document.createElement('input');
+    input_bpm.setAttribute('id', 'input_bpm');
+    input_ar.setAttribute('id', 'input_ar');
+    input_cs.setAttribute('id', 'input_cs');
+    input_od.setAttribute('id', 'input_od');
+    input_hp.setAttribute('id', 'input_hp');
 
     form.appendChild(input_bpm);
     form.appendChild(input_ar);
@@ -19,9 +29,14 @@ function display_supported() {
     form.appendChild(input_od);
     form.appendChild(input_hp);
 
+    var download_button = document.createElement('button');
+    download_button.setAttribute('id', 'download_button');
+    download_button.addEventListener('click', handleDownload);
+
     root_div.setAttribute('title', 'placeholder');
     root_div.className = "supported";
     root_div.appendChild(form);
+    root_div.appendChild(download_button);
 }
 
 async function getCurrentTab() {
@@ -32,6 +47,8 @@ async function getCurrentTab() {
 }
 
 let match_osu_url = new RegExp("^https:\/\/osu\.ppy\.sh\/beatmapsets\/[0-9]+#(osu|fruits|taiko|mania)\/[0-9]+$");
+let beatmap_download_url = "https://osu.ppy.sh/beatmapsets/"
+
 
 async function main() {
     const tab = await getCurrentTab();
@@ -39,6 +56,7 @@ async function main() {
 
     if (match_osu_url.test(url)) {
         display_supported();
+
     } else {
         display_unsupported();
     }
